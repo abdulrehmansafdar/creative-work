9d5b054000c2d1dd0333be65365e9dc9bb320152
Abdul Rehman <145101831+abdulrehmansafdar@users.noreply.github.com>
2024-02-06 02:12:54 +0500

    airline website

---
+++ main.js
@@ -0,0 +1,16 @@
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
